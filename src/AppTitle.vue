a4f404e4f9f1703415c73b182edf4b11cd1abbdb
ericaostini <ericaostini14@gmail.com>
2023-11-08 15:13:15 +0100

    delete vue file HelloWorld and change file App.vue in AppTitle.vue

---
+++ src/AppTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    nome: AppTitle,
+    components: {
+
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
